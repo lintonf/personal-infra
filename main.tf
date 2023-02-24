d976e9b1959e11758dcd3bed70e9c2f61d0e7f88
Linton Fisher <8694778+lintonf@users.noreply.github.com>
2023-02-24 19:34:05 +0000

    Change backend to TF Cloud

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
 terraform {
-  backend "s3" {
-    bucket         = "287095433380-terraform"
-    dynamodb_table = "287095433380-terraform"
-    key            = "personal-infra/terraform.tfstate"
-    region         = "eu-west-1"
+  cloud {
+    organization = "lintonf"
+
+    workspaces {
+      name = "personal-infra"
+    }
   }
 }
 
